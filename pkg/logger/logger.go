ff02c83cbb858b56064b651d227d655b79b45f41
fancurson <fancurson@gmail.com>
2025-02-23 22:13:04 +0300

    Logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,7 +1,40 @@
 package logger
 
+import (
+	"context"
+	"fmt"
+
+	"go.uber.org/zap"
+)
+
+const (
+	Key = "logger"
+
+	RequestId = "request_id"
+)
+
 type Logger struct {
+	l *zap.Logger
 }
 
-func NewLogger() {
+func New(ctx context.Context) (context.Context, error) {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		return nil, fmt.Errorf("register logger error: %w", err)
+	}
+
+	ctx = context.WithValue(ctx, Key, &Logger{logger})
+	return ctx, err
+}
+
+func GetLoggerFromCtx(ctx context.Context) *Logger {
+	return ctx.Value(Key).(*Logger)
+}
+
+func (l *Logger) Info(ctx context.Context, msg string, fields ...zap.Field) {
+	if ctx.Value(RequestId) != nil {
+		fields = append(fields, zap.String(RequestId, ctx.Value(RequestId).(string)))
+	}
+
+	l.l.Info(msg, fields...)
 }
